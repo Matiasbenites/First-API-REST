5cd85e5dfe1a8a9b0bd0615f97f911f9237c023e
Matute <79936247+Matiasbenites@users.noreply.github.com>
2022-12-30 15:02:55 -0300

    Rol's endpoints finished

--- homework/routes/roles.js
+++ homework/routes/roles.js
@@ -1,10 +1,52 @@
 const express = require("express");
+const rolModel = require("../models/roles");
+
 const rolRoutes = express.Router();
 
+//Default route for return existing roles
+rolRoutes.get("/", async (req, res) => {
+  const data = await rolModel.findAll();
 
-//Define of a route
-rolRoutes.get("/roles", (req, res) => {
-  res.json({ status: 200, data: {} });
+  res.json({ status: 200, data: data });
+});
+
+//Route for find by one rol
+rolRoutes.get("/:id", async (req, res) => {
+  const data = await rolModel.findOne(req.body, {
+    where: { id: req.params.id },
+  });
+
+  res.json({ status: 200, data: data });
+});
+
+//Route for create a new role
+rolRoutes.post("/create", async (req, res) => {
+  const data = await rolModel.create(req.body);
+
+  res.json({ status: 200, data: data });
+});
+
+//Route for update an existing role
+rolRoutes.put("/:id", async (req, res) => {
+  const data = await rolModel.update(req.body, {
+    where: { id: req.body.id },
+  });
+
+  res.json({ status: 200, data: data });
+});
+
+//Route for delete an existing role
+
+rolRoutes.delete("/:id", async (req, res) => {
+  console.log("id", req.params.id);
+
+  rolModel
+    .destroy({
+      where: { id: req.params.id },
+    })
+    .then((data) => {
+      res.json({ status: 200, data });
+    });
 });
 
 module.exports = rolRoutes;
