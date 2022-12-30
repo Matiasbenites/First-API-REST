dbe95f5abbce6fd09be48d0557977d8845c4f712
Matute <79936247+Matiasbenites@users.noreply.github.com>
2022-12-30 15:02:32 -0300

    Added some custom logs

--- homework/server.js
+++ homework/server.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const apiRoutes = require("./routes");
-const db = require("./models");
+const db = require('./models')
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api", apiRoutes);
 
-db.sync()
-  .then(() => {
-    console.log("Conectado a SQLITE!!!");
-  })
-  .catch(() => {
-    console.log("Se produjo un error al intentar conectarse a SQLITE");
-  });
+
+app.use("/api", apiRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello World! 💫💫");
+});
+
+db.sync().then(() => {
+  console.log("Conectado a SEQUELIZE 💠💠💠")
+}).catch(() => {
+  console.log("Se produjo un error al intentar conectarse a SEQUELIZE 💥💥💥 ")
+});
 
 app.listen(3000, "localhost", () => {
-  console.log("Servidor funcionando correctamente en puerto 3000");
+  console.log("Servidor funcionando correctamente en puerto 3000  💫💫💫");
 });
