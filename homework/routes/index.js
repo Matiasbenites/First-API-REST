5dacea4afe544134fe129066621c280165af51de
Matute <79936247+Matiasbenites@users.noreply.github.com>
2022-12-30 10:50:30 -0300

    Comments added

--- homework/routes/index.js
+++ homework/routes/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const userRoutes = require('./users');
-const rolRoutes = require('./roles');
-const taskRoutes = require('./tasks');
+const express = require("express");
+const userRoutes = require("./users");
+const rolRoutes = require("./roles");
+const taskRoutes = require("./tasks");
 
-const apiRoutes = express.Router();
+const apiRoutes = express.Router(); // Main route
 
+// Call to routes one by one
 
 apiRoutes.use("/users", userRoutes);
 
